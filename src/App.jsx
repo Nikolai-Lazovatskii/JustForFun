919547de10879d62a1398fc00360b360c14c6282
Nicolas_Ray <nicolas.lazov25@mail.ru>
2023-09-13 23:24:58 +0200

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // Define the set of characters to be used
   const symbols =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+  // Initialize state for the matrix of symbols, filling it with '#'
   const [currentSymbols, setCurrentSymbols] = useState(
     Array(20).fill("#".repeat(20))
   );
 
+  // Handler for the mouse move event to update the symbols
   const mouseMoveHandler = () => {
     let newSymbols = currentSymbols.map(() => {
       let newRow = "";
@@ -23,21 +27,25 @@ function App() {
   };
 
   useEffect(() => {
+    // Initialize clipboard to handle copy functionality
     const clipboard = new ClipboardJS(".copyButton", {
       text: function () {
         return currentSymbols.join("\n");
       },
     });
 
+    // Log success message when copy is successful
     clipboard.on("success", function (e) {
       console.log("Successfully copied:", e.text);
       e.clearSelection();
     });
 
+    // Log error message when copy fails
     clipboard.on("error", function (e) {
       console.error("Error^ problems with copied", e);
     });
     
+    // Cleanup clipboard listener when component is unmounted
     return () => {
       clipboard.destroy()
     }
@@ -46,6 +54,7 @@ function App() {
   return (
     <div onMouseMove={mouseMoveHandler}>
       <div className="textBox">
+        {/* Map through current symbols and display them */}
         {currentSymbols.map((row, index) => (
           <div key={index}>
             {row.split("").map((el, i) => (
@@ -55,6 +64,7 @@ function App() {
         ))}
       </div>
       <div className="copyContainer">
+        {/* Button to trigger copy functionality */}
         <button className="copyButton">copy</button>
       </div>
     </div>
